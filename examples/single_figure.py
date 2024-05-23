a0183f67d62e8794a24eb9c9d97a24d40869b74e
Katherine <krosenf@gmail.com>
2024-05-23 09:23:36 -0700

    update

--- examples/single_figure.py
+++ examples/single_figure.py
@@ -1,31 +1,33 @@
-""" single_figure.py
+"""single_figure.py
 Create a single figure with defined axis and margin size.
 """
+
 import pyfcf
 import matplotlib.pyplot as plt
 from pathlib import Path
+pyfcf.FigConfig.setup_matplotlib(font_size=14)
 
-x = [1,2,3,4]
-y = [1,1,2,5]
+def single_figure_example():
+    x = [1, 2, 3, 4]
+    y = [1, 1, 2, 5]
 
-def example1():
+    xm = [0.7, 0.05]  # x margin
+    ym = [0.4, 0.3]  # y margin
 
-    xm = [0.4, 0.05] # x margin
-    ym = [0.4, 0.3] # y margin
-
-    fc = pyfcf.FigConfig(1,1, xm=xm, ym=ym, idx=5, idy=3)
+    fc = pyfcf.FigConfig(1, 1, xm=xm, ym=ym, idx=5, idy=3)
     print(fc.xs, fc.ys)
 
     plt.figure(figsize=(fc.xs, fc.ys))
     rect = fc.get_rect(0, 0)
     print(rect)
-    
-    ax = plt.axes(rect)
-    ax.plot(x,y,'-o')
-    ax.set_title('Title')
 
-    plt.savefig(Path(__file__).resolve().parent / 'single_figure.png')
+    ax = plt.axes(rect)
+    ax.plot(x, y, "-o")
+    ax.set_ylabel("Y-axis")
+    ax.set_title("Title")
+
+    plt.savefig(Path(__file__).resolve().parent / f"{fc.get_script_name(__file__)}.png")
+
 
 if __name__ == "__main__":
-
-    example1()
+    single_figure_example()
