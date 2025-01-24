76a9da885e281dfa9744eee39d497fe212aa8de4
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-01-24 02:05:47 +0000

    add version

--- pyfcf/__init__.py
+++ pyfcf/__init__.py
@@ -1,1 +1,3 @@
+from .version import __version__, __versiondate__, __license__
+
 from .basic import *
---
+++ pyfcf/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ["__version__", "__versiondate__", "__license__"]
+
+__version__ = "0.1.1"
+__versiondate__ = "2025-01-23"
+__license__ = "MIT"
