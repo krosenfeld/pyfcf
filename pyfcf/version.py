e270e88527d3fa219ae5b52eb38864aa79e75585
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-01-24 02:13:27 +0000

    play with quotes

--- pyfcf/version.py
+++ pyfcf/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
 __version__ = "0.1.1"
-__versiondate__ = "2025-01-20"
+__versiondate__ = '2025-01-20'
 __license__ = "MIT"
